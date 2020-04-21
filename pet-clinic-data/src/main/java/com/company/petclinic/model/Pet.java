06e8bb388344dedde80db8b9a6ac71ab81888bd7
nubranger <mtviraga@gmail.com>
2020-04-21 16:54:33 +0300

    refactoring to common crud interfaces

--- pet-clinic-data/src/main/java/com/company/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/company/petclinic/model/Pet.java
@@ -2,7 +2,7 @@ package com.company.petclinic.model;
 
 import java.time.LocalDate;
 
-public class Pet {
+public class Pet extends BaseEntity {
 
     private String name;
     private PetType petType;
--- pet-clinic-data/src/main/java/com/company/petclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/com/company/petclinic/services/PetService.java
@@ -2,13 +2,6 @@ package com.company.petclinic.services;
 
 import com.company.petclinic.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet, Long>{
 
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
 }
