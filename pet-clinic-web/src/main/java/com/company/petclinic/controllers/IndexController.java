c1a8150695c392bfa1178b0dc729c3d56adeb9aa
nubranger <mtviraga@gmail.com>
2020-04-21 17:53:32 +0300

    adding index page and controller

---
+++ pet-clinic-web/src/main/java/com/company/petclinic/controllers/IndexController.java
@@ -0,0 +1,14 @@
+package com.company.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"","/","index","index.htm"})
+    public String index(){
+
+        return "index";
+    }
+}
